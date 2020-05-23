90f2e8284e4080d80fc03d92dd101387e82e27e7
dnorkett <dnorkett@gmail.com>
2020-05-23 15:08:29 -0500

    updated router

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = 3000;
+const cors = require('cors');
 require('dotenv/config');
 
-app.use(bodyParser.json());
 
 //Import Routes
 const postsRoute = require('./routes/posts');
+
+
 //Middlewear
 //Function that executes when routes are being hit
 //Parameters - route, callback function
+app.use(cors());
+app.use(bodyParser.json());
 app.use('/posts', postsRoute);
-
 
 
 app.get('/', (req, res) => {
--- routes/posts.js
+++ routes/posts.js
@@ -5,16 +5,28 @@ const Post = require('../models/Post');
 
 //ROUTES
 //First parameter is route, second is callback function
-router.get('/', (req,res) => {
-    res.send('We are on posts');
+router.get('/', async (req,res) => {
+    try{
+        const posts = await Post.find();
+        res.json(posts);
+    }catch(err) {
+        res.json({message:err});
+    }
 });
 
 
-router.get('/specific', (req,res) => {
-    res.send('We are on a specific post');
+//Gets back a specific post
+router.get('/:postId', async (req,res) => {
+    try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+    }catch(err){
+        res.json({message:err});
+    }
 });
 
 
+//Submits a post
 router.post('/', (req, res) => {
     const post = new Post({
         title: req.body.title,
@@ -28,6 +40,30 @@ router.post('/', (req, res) => {
         .catch(err => {
             res.json({message: err});
         });
+});
+
+
+//Delete a post
+router.delete('/:postId', async (req,res) => {
+    try {
+    const removedPost = await Post.remove({_id: req.params.postId});
+    res.json(removedPost);
+    } catch(err){
+        res.json({message:err});
+    }
+});
+
+
+//Update a post
+router.patch('/:postId', async (req, res) => {
+    try{
+        const updatedPost = await Post.updateOne({_id: req.params.postId }, {$set: 
+            {title: req.body.title}}
+            );
+            res.json(updatedPost)        ;
+    }catch(err) {
+        res.json({message: err});
+    }
 });
 
 
